afdab8e65d7488f2dae0c0a64946cbdc58fd979b
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-01-06 13:37:17 +0900

    各モデルと必要な-mscfの生成

---
+++ app/Models/OvertimeSheet.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OvertimeSheet extends Model
+{
+    /** @use HasFactory<\Database\Factories\OvertimeSheetFactory> */
+    use HasFactory;
+}
