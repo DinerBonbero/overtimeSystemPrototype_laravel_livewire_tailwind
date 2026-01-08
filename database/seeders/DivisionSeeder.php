c5b9ff78a33ea6e4e55fb1b88c34f2a5d446b10a
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-01-08 14:36:16 +0900

    部署のマスタシーダ作成

--- database/seeders/DivisionSeeder.php
+++ database/seeders/DivisionSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DivisionSeeder extends Seeder
 {
@@ -12,6 +13,29 @@ class DivisionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('divisions')->insert([
+
+            [
+                'name' => '営業部'
+            ],
+            [
+                'name' => '人事部'
+            ],
+            [
+                'name' => '総務部'
+            ],
+            [
+                'name' => '経理部'
+            ],
+            [
+                'name' => '広報部'
+            ],
+            [
+                'name' => '法務部'
+            ],
+            [
+                'name' => 'IT部'
+            ]
+        ]);
     }
 }
