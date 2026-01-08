ca9b2d344100092278e445227712996f82a9a717
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-01-08 14:36:59 +0900

    役割(役職)のマスタシーダ作成

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,20 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('roles')->insert([
+
+            [
+                'name' => '一般社員'
+            ],
+            [
+                'name' => '次長'
+            ],
+            [
+                'name' => '課長'
+            ],
+            [
+                'name' => '部長'
+            ]
+        ]);
     }
 }
