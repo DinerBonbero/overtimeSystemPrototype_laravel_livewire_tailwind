f299cdf3865a36d2e59bf1ffaad76e5c510c5f3b
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-01-12 10:31:22 +0900

    役職の権限を記述

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        
+        Gate::define('general_employee', function (User $user) {//一般社員の権限
+            return $user->role_id === 1;
+        });
+
+        Gate::define('deputy_manager', function (User $user) {//課長の権限
+            return $user->role_id === 2;
+        });
+
+        Gate::define('section_manager', function (User $user) {//次長・部長の権限
+            return $user->role_id === 3 || $user->role_id === 4;
+        });
     }
 }
