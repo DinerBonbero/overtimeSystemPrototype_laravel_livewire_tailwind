0e90a31dcd7daee7b842ca0df6d23dba29ed877d
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-01-14 16:00:01 +0900

    コメント記述

--- resources/views/components/ui/button.blade.php
+++ resources/views/components/ui/button.blade.php
@@ -1,1 +1,2 @@
-<button type="button" {{ $attributes->merge(['class' => 'text-white font-font-mono cursor-pointer border-2 border-gray-300 rounded-lg']) }}>{{ $message }}</button>
+<button {{ $attributes->merge(['class' => 'text-white font-font-mono cursor-pointer border-2 border-gray-300 rounded-lg']) }}>{{ $message }}</button>
+{{-- type="button"に要注意、時間ロス --}}
