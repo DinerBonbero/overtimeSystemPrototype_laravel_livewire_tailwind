6d140632f171bd9a8e506277ea9dcba547eaa701
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-01-19 23:44:18 +0900

    レイアウト調整

--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -6,7 +6,7 @@
 </head>
 
 <body class="min-h-screen bg-amber-50">
-    <div class="mx-auto max-w-7xl text-center">
+    <div class="mx-auto max-w-7xl text-center pt-50">
         <div class="p-10 my-10 bg-white text-base sm:text-2xl font-serif text-gray-800">
             <p class="mb-3">予期しないエラーです。</p>
             <p>現在、問題が発生しています。</p>
