6a92bd2b8c27ce78338abe6ce66b902c910722c1
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-02-13 22:34:10 +0900

    レイアウト修正：表示テキストの入力と表・ボタンの位置

--- resources/views/overtime_sheets/show.blade.php
+++ resources/views/overtime_sheets/show.blade.php
@@ -49,43 +49,8 @@
             <table class="m-auto mt-6 border-collapse w-full text-center">
                 <tr>
                     <td class="border border-black px-2 py-1 w-1/4">残業予定時間</td>
-                    <td class="border border-black px-2 py-1 w-3/4 text-xs">
-                        <input type="date" name="plan_date">
-                        <select name="plan_start_hour" id="plan_start_hour">
-                            @for ($i = 0; $i <= 23; $i++)
-                                @php $planStartHour = str_pad($i, 2, 0, STR_PAD_LEFT); @endphp
-                                <option value="{{ $planStartHour }}" @if (old('plan_start_hour') == $planStartHour) selected @endif>
-                                    {{ $planStartHour }}</option>
-                                {{-- str_pad($i, 2, 0, STR_PAD_LEFT)は2桁になるように左側に0を追加する関数で返り値は文字列 --}}
-                                {{-- ループ変数に関数の戻り値を代入したりしない$i = str_pad($i, 2, 0, STR_PAD_LEFT)にしてしまうと$iが整数型から文字列型に変わりバグの原因になる --}}
-                                {{-- コメント内の出力括弧に気を付ける、エンコードエラー --}}
-                            @endfor
-                        </select>
-                        <label for="plan_start_hour">時</label>
-                        <select name="plan_start_minute" id="plan_start_minute" class="bg-gray-200 border">
-                            @for ($i = 0; $i <= 45; $i += 15)
-                                @php $planStartMinute = str_pad($i, 2, 0, STR_PAD_LEFT); @endphp
-                                <option value="{{ $planStartMinute }}"
-                                    @if (old('plan_start_minute') == $planStartMinute) selected @endif>{{ $planStartMinute }}
-                                </option>
-                            @endfor
-                        </select>
-                        <label for="plan_start_minute">分</label>
-                        <span>～</span>
-                        <select name="plan_end_hour" id="plan_end_hour">
-                            @for ($i = 0; $i <= 23; $i++)
-                                @php $planEndHour = str_pad($i, 2, 0, STR_PAD_LEFT); @endphp
-                                <option value="{{ $planEndHour }}" @if (old('plan_end_hour') == $planEndHour) selected @endif>
-                                    {{ $planEndHour }}</option>
-                            @endfor
-                        </select><label for="plan_end_hour">時</label>
-                        <select name="plan_end_minute" id="plan_end_minute">
-                            @for ($i = 0; $i <= 45; $i += 15)
-                                @php $planEndMinute = str_pad($i, 2, 0, STR_PAD_LEFT);@endphp
-                                <option value="{{ $planEndMinute }}" @if (old('plan_end_minute') == $planEndMinute) selected @endif>
-                                    {{ $planEndMinute }}</option>
-                            @endfor
-                        </select><label for="plan_end_minute">分</label>
+                    <td class="border border-black px-2 py-1 w-3/4">
+                        年 月 日 時 分 ～ 年 月 日 時 分
                     </td>
                 </tr>
             </table>
@@ -121,11 +86,7 @@
             <!-- 残業申登録録ボタン -->
             <div class="text-center mt-6">
                 <x-ui.button message="編集する" class="bg-blue-600 hover:bg-blue-700 py-1 px-2" />
-            </div>
-            <div class="text-center mt-6">
                 <x-ui.button message="申請する" class="bg-blue-600 hover:bg-blue-700 py-1 px-2" />
-            </div>
-            <div class="text-center mt-6">
                 <x-ui.button message="再申請を依頼する" class="bg-blue-600 hover:bg-blue-700 py-1 px-2" />
             </div>
 
@@ -135,51 +96,24 @@
 
             <table class="m-auto mt-6 border-collapse w-full text-center">
                 <tr>
-                    <td class="border border-black px-2 py-1 w-1/4">残業予定時間</td>
-                    <td class="border border-black px-2 py-1 w-3/4 text-xs">
-                        <input type="date" name="plan_date">
-                        <select name="plan_start_hour" id="plan_start_hour">
-                            @for ($i = 0; $i <= 23; $i++)
-                                @php $planStartHour = str_pad($i, 2, 0, STR_PAD_LEFT); @endphp
-                                <option value="{{ $planStartHour }}" @if (old('plan_start_hour') == $planStartHour) selected @endif>
-                                    {{ $planStartHour }}</option>
-                                {{-- str_pad($i, 2, 0, STR_PAD_LEFT)は2桁になるように左側に0を追加する関数で返り値は文字列 --}}
-                                {{-- ループ変数に関数の戻り値を代入したりしない$i = str_pad($i, 2, 0, STR_PAD_LEFT)にしてしまうと$iが整数型から文字列型に変わりバグの原因になる --}}
-                                {{-- コメント内の出力括弧に気を付ける、エンコードエラー --}}
-                            @endfor
-                        </select>
-                        <label for="plan_start_hour">時</label>
-                        <select name="plan_start_minute" id="plan_start_minute" class="bg-gray-200 border">
-                            @for ($i = 0; $i <= 45; $i += 15)
-                                @php $planStartMinute = str_pad($i, 2, 0, STR_PAD_LEFT); @endphp
-                                <option value="{{ $planStartMinute }}"
-                                    @if (old('plan_start_minute') == $planStartMinute) selected @endif>{{ $planStartMinute }}
-                                </option>
-                            @endfor
-                        </select>
-                        <label for="plan_start_minute">分</label>
-                        <span>～</span>
-                        <select name="plan_end_hour" id="plan_end_hour">
-                            @for ($i = 0; $i <= 23; $i++)
-                                @php $planEndHour = str_pad($i, 2, 0, STR_PAD_LEFT); @endphp
-                                <option value="{{ $planEndHour }}" @if (old('plan_end_hour') == $planEndHour) selected @endif>
-                                    {{ $planEndHour }}</option>
-                            @endfor
-                        </select><label for="plan_end_hour">時</label>
-                        <select name="plan_end_minute" id="plan_end_minute">
-                            @for ($i = 0; $i <= 45; $i += 15)
-                                @php $planEndMinute = str_pad($i, 2, 0, STR_PAD_LEFT);@endphp
-                                <option value="{{ $planEndMinute }}" @if (old('plan_end_minute') == $planEndMinute) selected @endif>
-                                    {{ $planEndMinute }}</option>
-                            @endfor
-                        </select><label for="plan_end_minute">分</label>
+                    <td class="border border-black px-2 py-1 w-1/4">実残業時間</td>
+                    <td class="border border-black px-2 py-1 w-3/4">
+                        年 月 日 時 分 ～ 年 月 日 時 分
+                    </td>
+                </tr>
+            </table>
+            <table class="m-auto mt-6 border-collapse w-full text-center">
+                <tr>
+                    <td class="border border-black px-2 py-1 w-1/4">うち休憩時間</td>
+                    <td class="border border-black px-2 py-1 w-3/4">
+                        時間 分
                     </td>
                 </tr>
             </table>
             <table class="mt-6 border-collapse w-full text-center">
                 <tr>
                     <td class="border border-black px-2 py-1">
-                        <span>残業理由</span>
+                        <span>残業報告</span>
                     </td>
                 </tr>
                 <tr>
@@ -191,28 +125,20 @@
             <table class="mt-6 border-collapse w-full text-center">
                 <tr>
                     <td class="border border-black px-2 py-1">
-                        <span>上記の残業を命令します</span>
+                        <span>上記の残業を報告します</span>
                     </td>
                 </tr>
                 <tr>
                     <td class="border border-black px-2 py-1">
-
                         <span>年月日</span>
-
-                        <span>承認者</span>
-
-                        <span>※保留</span>
+                        <span>申請者</span>
                     </td>
                 </tr>
             </table>
             <!-- 残業報告登録ボタン -->
             <div class="text-center mt-6">
                 <x-ui.button message="編集する" class="bg-blue-600 hover:bg-blue-700 py-1 px-2" />
-            </div>
-            <div class="text-center mt-6">
                 <x-ui.button message="未提出にする" class="bg-blue-600 hover:bg-blue-700 py-1 px-2" />
-            </div>
-            <div class="text-center mt-6">
                 <x-ui.button message="印刷" class="bg-blue-600 hover:bg-blue-700 py-1 px-2" />
             </div>
         </div>
