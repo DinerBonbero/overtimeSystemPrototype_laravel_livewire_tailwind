8033486481974ede58a86d164a9631a1b2c38a24
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-02-14 22:50:11 +0900

    datetime:H:i:sにキャスト：start_time、end_time ※bladeのformat('H:i')に合わせた

--- app/Models/WorkPattern.php
+++ app/Models/WorkPattern.php
@@ -6,10 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class WorkPattern extends Model
 {
-    
+
     protected $guarded = [
         'id'
     ];
+
+    protected function casts(): array //キャスト
+    {
+        return [
+            'start_time' => 'datetime:H:i:s',//時間だけを保存するため、H:i:sでキャスト
+            'end_time' => 'datetime:H:i:s',//時間だけを保存するため、H:i:sでキャスト
+        ];
+    }
 
     public function overtimeRequests()
     {
