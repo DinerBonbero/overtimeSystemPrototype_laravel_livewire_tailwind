d5326f40453163358c399c26adaf67dc3353c388
ugokusenseinakashi <jigokusenseinakashi@gmail.com>
2026-02-19 18:55:41 +0900

    レイアウトの大枠を作成

---
+++ resources/views/overtime_sheets/edit.blade.php
@@ -0,0 +1,93 @@
+<x-layouts.app.header>
+
+    <body>
+        <div class="container w-7/11 mx-auto text-base">
+            <!-- タイトル -->
+            <div class="text-center mt-6 text-2xl">
+                <h1>残業申請</h1>
+            </div>
+
+            <!-- 残業申請 -->
+            <div class="pt-2 mx-auto grid grid-cols-7 w-full">
+                <div class="col-start-1 col-span-3">
+                    <table class="border-collapse">
+                        <tr class="border border-black w-full">
+                            <td class="py-1 px-2 border border-black text-center">
+                                部署名：{{ $overtimeSheet->division->name }}
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-end-8 col-span-3">
+                    <table class="border-collapse ml-auto">
+                        <tr class="border border-black w-full">
+                            <td class="py-1 px-2 border border-black">
+                                社員番号：{{ Auth::user()->employee_number }}
+                            </td>
+                            <td class="py-1 px-2 text-center">
+                                名前：{{ Auth::user()->name }}
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <table class="m-auto mt-6 border-collapse w-full text-center">
+                <tr>
+                    <td class="border border-black px-2 py-1 w-1/4">勤務パターン</td>
+                    <td class="border border-black px-2 py-1 w-3/4 text-center">
+                        {{ $overtimeSheet->overtimeRequest->workPattern->name }}
+                        &nbsp;&nbsp;|&nbsp;&nbsp;
+                        {{-- ノーブレイクスペースブラウザに消されないスペース --}}
+                        {{ $overtimeSheet->overtimeRequest->workPattern->start_time->format('H:i') }} ～
+                        {{ $overtimeSheet->overtimeRequest->workPattern->end_time->format('H:i') }}
+                    </td>
+                </tr>
+            </table>
+            <table class="m-auto mt-6 border-collapse w-full text-center">
+                <tr>
+                    <td class="border border-black px-2 py-1 w-1/4">残業予定時間</td>
+                    <td class="border border-black px-2 py-1 w-3/4">
+                        {{ $overtimeSheet->overtimeRequest->plan_start_at->format('Y年m月d日 H時i分') }}
+                        ～
+                        {{ $overtimeSheet->overtimeRequest->plan_end_at->format('Y年m月d日 H時i分') }}
+                    </td>
+                </tr>
+            </table>
+            <table class="mt-6 border-collapse w-full text-center">
+                <tr>
+                    <td class="border border-black px-2 py-1">
+                        <span>残業理由</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="border border-black px-3 py-1">
+                        {{ $overtimeSheet->overtimeRequest->cause }}
+                    </td>
+                </tr>
+            </table>
+            <table class="mt-6 border-collapse w-full text-center">
+                <tr>
+                    <td class="border border-black px-2 py-1">
+                        <span>上記の残業を命令します</span>
+                    </td>
+                </tr>
+                <tr class="border border-black">
+                    <td class="px-2 py-1 grid grid-cols-9">
+                        <span class="col-start-1">年月日{{ $overtimeSheet->overtimeRequest->approved_at ?? '' }}</span>
+                        <span class="col-end-7">承認者{{ $overtimeSheet->overtimeRequest->user->name ?? '' }}</span>
+                        
+                    </td>
+                </tr>
+            </table>
+            <!-- 残業申登録録ボタン -->
+            <div class="mt-6 flex justify-center gap-3">
+                <form action="{{ route('overtime_sheets.update', $overtimeSheet->overtimeRequest->id) }}" method="post">
+                    @csrf
+                    @method('PUT')
+                    <x-ui.button message="更新する" class="bg-green-600 hover:bg-green-700 py-1 px-2"/>
+                </form>
+            </div>
+        </div>
+    </body>
+
+</x-layouts.app.header>
